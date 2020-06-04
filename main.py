db3eb7b988fd09b968a57add663d6f6ff6207584
Juan Casilla <juan.casilla@datosmeg.com>
2020-06-03 22:59:20 -0400

    main endpoint with index.html and static file serving

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+#!/usr/bin/env python3
+
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__, static_url_path = '', static_folder = 'client')
+
+with open('client/index.html', 'r') as f:
+    index_html = f.read()
+
+@app.route('/')
+def index():
+    return index_html
+
+if __name__ == '__main__':
+    app.run(port = 8080, debug = True)
